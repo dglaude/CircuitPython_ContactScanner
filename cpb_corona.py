ceb286c431f167200061b3ea9c1b10f334b370bb
David Glaude <dglaude@users.noreply.github.com>
2020-10-05 20:56:19 +0200

    Update to the version on my CPB
    Simplifying the code by trying to remove unused piece from the CLUE version.
    Turning to blue phone (mac) that do not advertise since mode than 20s, only remove after 200s.
    Working on keeping the same phone (mac) at the same position rather than sorted by power.
    Still a WIP.

--- cpb_corona.py
+++ cpb_corona.py
@@ -5,6 +5,7 @@
 
 ### Tested with Circuit Playground Bluefruit:
 ### Adafruit CircuitPython 6.0.0-alpha.2 on 2020-07-23; Adafruit Circuit Playground Bluefruit with nRF52840
+### Adafruit CircuitPython 6.0.0-beta.1 on 2020-10-01; Adafruit Circuit Playground Bluefruit with nRF52840
 
 ### Copyright (c) 2020 David Glaude
 ### Simplified version to remove the need for TFT Gizmo and work only with the 10 NeoPixel of the CPB
@@ -44,6 +45,7 @@ import digitalio
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import Advertisement
 
+
 ### Inputs (buttons reversed as it is used upside-down with Gizmo)
 _button_a = digitalio.DigitalInOut(board.BUTTON_A)
 _button_a.switch_to_input(pull=digitalio.Pull.DOWN)
@@ -52,17 +54,20 @@ _button_b.switch_to_input(pull=digitalio.Pull.DOWN)
 button_left = lambda: _button_b.value
 button_right = lambda: _button_a.value
 
+# The number of rows is also the number of NEOPIXEL
+rows = 10
+NO_IDX = -1
+
+
 BRIGHTNESS = 0.3
 BLUE = (0, 0, 255)
 OFF = (0, 0, 0)
-strip = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=BRIGHTNESS)
+strip = neopixel.NeoPixel(board.NEOPIXEL, rows, brightness=BRIGHTNESS)
 strip.fill(BLUE)
 time.sleep(1)
 strip.fill(OFF)
 
-rows = 10
-
-debug = 1
+debug = 0
 
 def d_print(level, *args, **kwargs):
     """A simple conditional print for debugging based on global debug level."""
@@ -71,89 +76,117 @@ def d_print(level, *args, **kwargs):
     elif debug >= level:
         print(*args, **kwargs)
 
-screen_update_ns = 250 * 1000 * 1000
 last_seen_update_ns = time.monotonic_ns()
 
-stale_time_ns = 65 * 1000 * 1000 * 1000
+screen_update_ns = 250 * 1000 * 1000
+
+hide_time_ns = 20 * 1000 * 1000 * 1000
+stale_time_ns = 200 * 1000 * 1000 * 1000
+
 scan_time_s = 10
 
 ble = BLERadio()
 ble.name = "CPB"
 
-count = 1
-
-complete_names_count = {}
 addresses_count = {}
-oui_count = {}
 
 ### An array of timestamp and advertisement by key (addr)
 last_ad_by_key = {}
 
-c_name_by_addr = {}
+### Observed value measured on CPB as advertised by my phones with CoronAlert app.
+### Extreem value so that you get full red or full green (-90 to -50 give less frequently pure color)
+MIN_RSSI = -80
+MAX_RSSI = -60
+NOT_RSSI = -127
+
 
 def remove_old(ad_by_key, expire_time_ns):
     """Delete any entry in the ad_by_key dict with a timestamp older than expire_time_ns."""
-    ### the list() is needed to make a real list from the iterator 
+    ### the list() is needed to make a real list from the iterator
     ### which allows modification of the dict inside the loop
     for key, value in list(ad_by_key.items()):
         if value[1] < expire_time_ns:
             del ad_by_key[key]
 
 
-### TODO: arg list is getting big here
-def gimme_color(disp, rows_g):
-    
-    return ( (255, 0, 0) )
+def hide_old(ad_by_key, hide_time_ns):
+    """Hide any entry in the ad_by_key dict with a timestamp older than hide_time_ns. (setting RSSI to -255)"""
+    ### the list() is needed to make a real list from the iterator
+    ### which allows modification of the dict inside the loop
+    for key, value in list(ad_by_key.items()):
+        if value[1] < hide_time_ns:
+            ad_by_key[key] = (value[0], value[1], NOT_RSSI, value[3])
 
 
-def update_screen(rows_n,
-                    ad_by_key,
-                    then_ns,
-                    tot_mac,
-                    tot_oui,
-                    tot_names,
-                    *,
-                    mem_free=None):
-    """Update the screen with the entries with highest RSSI, recenctly seen.
-       The text colour is used to indicate how recent.
-       """
+def byte_bounded(val):
+    return (min(max(round(val) , 0), 255))
 
-    if mem_free is None:
-        summary_text = "MACs:{:<4d}  OUIs:{:<4d}  Names:{:<4d}".format(tot_mac,
-                                                                       tot_oui,
-                                                                       tot_names)
+
+### Compute a color based on age_ns and rssi
+def gimme_color(age_ns, rssi):
+    ### power_age should be near 1 for recent data and decreasing toward zero for old data
+
+    if rssi == NOT_RSSI:
+        result_color = (0, 0, 31)
     else:
-        summary_text = "MACs:{:<4d}  OUIs:{:<4d}  Names:{:<4d} M:{:<3d}".format(tot_mac,
-                                                                                tot_oui,
-                                                                                tot_names,
-                                                                                round(mem_free/1024.0))
-    d_print(3,
-            summary_text)
+        power_age = 1 - (age_ns / hide_time_ns)
+        red = byte_bounded ( ( ( (rssi-MIN_RSSI) / (MAX_RSSI-MIN_RSSI) ) * 255 ) * power_age )
+        green = byte_bounded ( (255 - ( ( (rssi-MIN_RSSI) / (MAX_RSSI-MIN_RSSI) ) * 255 ) ) * power_age )
+        result_color=(red, green, 0)
+        d_print(1, "Age: ", power_age, " RSSI: ", rssi, " Color: ", result_color)
+    return ( result_color )
 
+
+def update_screen(rows_n, ad_by_key, then_ns):
+    """Update the screen with the most recently seen entries.
+       The text colour is used to indicate the power of the signal
+       """
 
     ### Sort by the RSSI field, then the time field
     sorted_data = sorted(ad_by_key.items(),
-                         key=lambda item: (item[1][2], item[1][1]),
+                         key=lambda item: (item[0]),  # MAC address
+#                         key=lambda item: (item[1][2], item[1][1]),  # RSSI then unique code
                          reverse=True)
+
+    mac_no_idx = {}
+    idx_in_use = {}
 
     ### Add the top N rows to to the screen
     ### the key is the mac address as text without any colons
     idx = 0
     for key, value in sorted_data[:rows_n]:
-        ad, ad_time_ns, rssi = value
-        ### Add the colon sepators to the string version of the MAC address
-        masked_mac = key
-        mac_text = ":".join([masked_mac[off:off+2] for off in range(0, len(masked_mac), 2)])
-        name = c_name_by_addr.get(key)
-        if name is None:
-            name = "?"
-        ### Must be careful not to exceed the fixed max_glyphs field size here (40)
-        ### This should be from 0 to about 65s-75s
-        age = 170 - (then_ns - ad_time_ns) / stale_time_ns * 170
-        brightness = min(max(round(85 + age * 2.4), 0), 255)
-        d_print(6, "Set ", idx, " to brightness: ", brightness)
-        strip[idx]=(0,0,brightness)
+        ad, ad_time_ns, rssi, index_col = value
+        if index_col == NO_IDX:
+            d_print(1, "Key: ", key, " as no index.")
+            mac_no_idx[key] = 1
+        else:
+            try:
+                idx_in_use[index_col] += 1
+            except KeyError:
+                idx_in_use[index_col] = 1
+
+    d_print(1, "mac_no_idx = ", mac_no_idx)
+    d_print(1, "idx_in_use = ", idx_in_use)
+
+
+
+    if button_left():
+        print(sorted_data)
+        while button_left():
+            pass
+
+
+    ### Add the top N rows to to the screen
+    ### the key is the mac address as text without any colons
+    idx = 0
+    for key, value in sorted_data[:rows_n]:
+        ad, ad_time_ns, rssi, index_col = value
+
+        age_ns = then_ns - ad_time_ns
+        pixel_color=gimme_color(age_ns, rssi)
+        strip[idx]=pixel_color
         idx += 1
+        # What if idx >= rows ??? Maybe this slide avoid that: sorted_data[:rows_n]
 
     #### Blank out any rows not populated with data
     if idx < rows_n:
@@ -163,74 +196,47 @@ def update_screen(rows_n,
 
 
 while True:
-    d_print(2, "Loop", count)
     for ad in ble.start_scan(minimum_rssi=-127, timeout=scan_time_s):
         now_ns = time.monotonic_ns()
 
         if 3 in ad.data_dict:
             if ad.data_dict[3] == b'o\xfd':
 
-                ##addr_b = ad.address.address_bytes
-                c_name = ad.complete_name
-                addr_text = "".join(["{:02x}".format(b) for b in reversed(ad.address.address_bytes)])
+#                print(dir(ad))
+#                print(ad.data_dict)
 
-                last_ad_by_key[addr_text] = (ad, now_ns, ad.rssi)
+                ##addr_b = ad.address.address_bytes
+                addr_text = "".join(["{:02x}".format(b) for b in reversed(ad.address.address_bytes)])
+                last_ad_by_key[addr_text] = (ad, now_ns, ad.rssi, NO_IDX)
 
                 try:
                     addresses_count[addr_text] += 1
                 except KeyError:
                     addresses_count[addr_text] = 1
 
-                oui = addr_text[:6]
-                try:
-                    oui_count[oui] += 1
-                except KeyError:
-                    oui_count[oui] = 1
-    
-                if c_name is not None:
-                    c_name_by_addr[addr_text] = c_name
-                    try:
-                        complete_names_count[c_name] += 1
-                    except KeyError:
-                        complete_names_count[c_name] = 1      
-
         if button_right():
             debug_mem_free = gc.mem_free()
             print("Memfree: ", debug_mem_free)
+            print("AddCount:", addresses_count)
+            print("MACS", len(addresses_count))
             while button_right():
                 pass
 
         if button_left():
-            print(
-                "MACS", len(addresses_count),
-                "OUI", len(oui_count),
-                "NAMES", len(complete_names_count))
+#            print(last_ad_by_key)
+#            print(ad.address, ad.rssi, ad.scan_response, ad.tx_power, ad.complete_name, ad.short_name)
             while button_left():
                 pass
 
         if now_ns - last_seen_update_ns > screen_update_ns:
             gc.collect()
-            mem_free = gc.mem_free()
-
-            update_screen(rows,
-                        last_ad_by_key,
-                        now_ns,
-                        len(addresses_count),
-                        len(oui_count),
-                        len(complete_names_count),
-                        mem_free=mem_free)
-
+            update_screen(rows, last_ad_by_key, now_ns)
             last_seen_update_ns = now_ns
 
-        d_print(4,
+        d_print(1,
                 ad.address, ad.rssi, ad.scan_response,
                 ad.tx_power, ad.complete_name, ad.short_name)
 
+    hide_old(last_ad_by_key, time.monotonic_ns() - hide_time_ns)
+
     remove_old(last_ad_by_key, time.monotonic_ns() - stale_time_ns)
-
-    d_print(2,
-            "MACS", len(addresses_count),
-            "OUI", len(oui_count),
-            "NAMES", len(complete_names_count))
-
-    count += 1
