474df73fae31d72130c458da3e9f2c2a4e31a81f
David Glaude <dglaude@users.noreply.github.com>
2020-10-12 01:44:59 +0200

    Simplified version for CPB
    Completely renewed RGB LED allocation logic, storing index and tracking LED still available.
    Fixing the color when very very close.
    All the debug d_print removed.
    Unused variable cleaned.

--- cpb_corona.py
+++ cpb_corona.py
@@ -34,74 +34,47 @@
 ### SOFTWARE.
 
 import time
-import gc
 import os
 
 import board
 import neopixel
-import digitalio
 
 ### https://github.com/adafruit/Adafruit_CircuitPython_BLE
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import Advertisement
 
 
-### Inputs
-_switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
-_switch.switch_to_input(pull=digitalio.Pull.UP)
-switch = lambda: _switch.value
-
-
-# The number of rows is also the number of NEOPIXEL
+### The number of rows is also the number of NEOPIXEL
 rows = 10
+### This is the value used when we don't know what RGB LED to use yet
 NO_IDX = -1
-
-
-### Observed value measured on CPB as advertised by my phones with CoronAlert app.
-### Extreem value so that you get full red or full green (-90 to -50 give less frequently pure color)
+### Address that do not advertise anymore get a very low RSSI to indicate that
 NOT_RSSI = -127
 
-
-BRIGHTNESS = 0.3
+### This is just to show then CPE start (or restart)
+BRIGHTNESS = 1
 strip = neopixel.NeoPixel(board.NEOPIXEL, rows, brightness=BRIGHTNESS)
 strip.fill((0, 0, 31))
 time.sleep(0.5)
 strip.fill((0, 0, 0))
 
-
+### If no advertisement received for 'hide_time_ns' that RGB LED turn BLUE and will be forgotten
+hide_time_ns =      20 * 1000 * 1000 * 1000
+### If no advertisement is received for 'stale_time_ns' that RGB LED is flushed for reuse
 stale_time_ns =    200 * 1000 * 1000 * 1000
-
 scan_time_s = 10
 
 ble = BLERadio()
 ble.name = "CPB"
 
-addresses_count = {}
-
 ### An array of timestamp and advertisement by key (addr)
 last_ad_by_key = {}
-
-
-debug = 0
-def d_print(level, *args, **kwargs):
-    """A simple conditional print for debugging based on global debug level."""
-    if not isinstance(level, int):
-        print(level, *args, **kwargs)
-    elif debug >= level:
-        print(*args, **kwargs)
-
-
-last_seen_update_ns = time.monotonic_ns()
-
-screen_update_ns =        250 * 1000 * 1000
-hide_time_ns =      20 * 1000 * 1000 * 1000
-
 
 MINI_BLUE = (0, 0, 1)
 SHADE_BLUE = [(0, 0, 63), (0, 0, 31), (0, 0, 15), (0, 0, 7), (0, 0, 3)]
 TIME_BLUE = [50 * 1000 * 1000 * 1000, 80 * 1000 * 1000 * 1000, 110 * 1000 * 1000 * 1000, 140 * 1000 * 1000 * 1000, 170 * 1000 * 1000 * 1000]
 
-RSSI_DEFAULT_COLOR = (0, 63, 0)
+RSSI_DEFAULT_COLOR = (63, 0, 0)
 RSSI_COLOR = [(0, 31, 0), (15, 31, 0), (15, 15, 0), (31, 15, 0), (31, 0, 0), (63, 0, 0)]
 RSSI_VALUE = [-80, -75, -70, -65, -60, -55]
 
@@ -123,12 +96,17 @@ def gimme_color(age_ns, rssi):
     return ( result_color )
 
 
-def remove_old(ad_by_key, expire_time_ns):
-    """Delete any entry in the ad_by_key dict with a timestamp older than expire_time_ns."""
-    ### the list() is needed to make a real list from the iterator
-    ### which allows modification of the dict inside the loop
-    for key, value in list(ad_by_key.items()):
-        if value[1] < expire_time_ns:
+def delete_very_old(rows_n, ad_by_key):
+    """Delete older key above the number of rows_n"""
+    ### If we have more entry than space
+    if len(ad_by_key)>rows_n:
+        ### Sort by last seen to identify earliest that should be cleaned
+        sorted_data = sorted(ad_by_key.items(), key=lambda item: (item[1][1]))
+        ### Number of entries to remove
+        to_delete = len(ad_by_key)-rows_n
+        ### Iterate on the first entry and delete them from the list received
+        for key, value in sorted_data[:to_delete]:
+            ### Delete such entry
             del ad_by_key[key]
 
 
@@ -141,89 +119,59 @@ def hide_old(ad_by_key, hide_time_ns):
             ad_by_key[key] = (value[0], value[1], NOT_RSSI, value[3])
 
 
-def delete_old(rows_n, ad_by_key):
-    """Delete older key above the number of rows_n"""
-    ### Sort by last seen to identify earliest that should be cleaned
-    sorted_data = sorted(ad_by_key.items(), key=lambda item: (item[1][1]))
-    ### Number of entries to remove
-    to_delete = len(ad_by_key)-rows_n
-    ### Iterate on the first entry and delete them from the list received
-    for key, value in sorted_data[:to_delete]:
-        ### Delete such entry
-        del ad_by_key[key]
+def remove_old(ad_by_key, expire_time_ns):
+    """Delete any entry in the ad_by_key dict with a timestamp older than expire_time_ns."""
+    ### the list() is needed to make a real list from the iterator
+    ### which allows modification of the dict inside the loop
+    for key, value in list(ad_by_key.items()):
+        if value[1] < expire_time_ns:
+            del ad_by_key[key]
 
 
 def update_screen(rows_n, ad_by_key, then_ns):
-    """Update the screen with the most recently seen entries.
-       The text colour is used to indicate the power of the signal
-       """
-
+    """Colour is used to indicate the power of the signal or absence or recent signal."""
+    possible = list(range(rows_n))
     ### Sort by the MAC field
     sorted_data = sorted(ad_by_key.items(), key=lambda item: (item[0]))
-
-    mac_no_idx = {}
-    idx_in_use = {}
-
-    ### Add the top N rows to to the screen
-    ### the key is the mac address as text without any colons
-    idx = 0
+    ### Scan all element and see what index are already in use
+    for key, value in sorted_data[:rows_n]:
+        ad, ad_time_ns, rssi, index_col = value
+        if index_col != NO_IDX:
+            possible.remove(index_col)
+    ### Scan all element and attribute index for those without one
     for key, value in sorted_data[:rows_n]:
         ad, ad_time_ns, rssi, index_col = value
         if index_col == NO_IDX:
-            d_print(1, "Key: ", key, " as no index.")
-            mac_no_idx[key] = 1
-        else:
-            try:
-                idx_in_use[index_col] += 1
-            except KeyError:
-                idx_in_use[index_col] = 1
-
-    d_print(1, "mac_no_idx = ", mac_no_idx)
-    d_print(1, "idx_in_use = ", idx_in_use)
-
-
-    ### Add the top N rows to to the screen
-    ### the key is the mac address as text without any colons
-    idx = 0
+            new_index=possible.pop()
+            ad_by_key[key] = (ad, ad_time_ns, rssi, new_index)
+    ### Scan all element to display the color
     for key, value in sorted_data[:rows_n]:
         ad, ad_time_ns, rssi, index_col = value
         age_ns = then_ns - ad_time_ns
         pixel_color=gimme_color(age_ns, rssi)
-        strip[idx]=pixel_color
-        idx += 1
-        # What if idx >= rows ??? Maybe this slide avoid that: sorted_data[:rows_n]
-
-    #### Blank out any rows not populated with data
-    if idx < rows_n:
-        for _ in range(rows_n - idx):
-            strip[_ + idx]=(0, 0, 0)
-            d_print(6, "Set ", _ + idx, " to off.")
+        strip[index_col]=pixel_color
+    ### Scan unused index to clear the color
+    for index in possible:
+        strip[index]=(0, 0, 0)
 
 
 while True:
+    ### For all the advertisement
     for ad in ble.start_scan(minimum_rssi=-127, timeout=scan_time_s):
+        ### Take the timestamp
         now_ns = time.monotonic_ns()
-
+        ### If this is a contact tracing advertisement
         if 3 in ad.data_dict:
             if ad.data_dict[3] == b'o\xfd':
+                ### Found a Contact Tracing advertisement.
                 addr_text = "".join(["{:02x}".format(b) for b in reversed(ad.address.address_bytes)])
-                last_ad_by_key[addr_text] = (ad, now_ns, ad.rssi, NO_IDX)
-                try:
-                    addresses_count[addr_text] += 1
-                except KeyError:
-                    addresses_count[addr_text] = 1
-
-
-        if now_ns - last_seen_update_ns > screen_update_ns:
-            gc.collect()
-            update_screen(rows, last_ad_by_key, now_ns)
-            last_seen_update_ns = now_ns
-
-        d_print(1,
-                ad.address, ad.rssi, ad.scan_response,
-                ad.tx_power, ad.complete_name, ad.short_name)
-
-    if len(last_ad_by_key)>rows:
-        delete_old(rows, last_ad_by_key)
-    hide_old(last_ad_by_key, time.monotonic_ns() - hide_time_ns)
-    remove_old(last_ad_by_key, time.monotonic_ns() - stale_time_ns)
+                if addr_text in last_ad_by_key:
+                    ### Updating existing entry with a new timestamp
+                    last_ad_by_key[addr_text] = (ad, now_ns, ad.rssi, last_ad_by_key[addr_text][3])
+                else:
+                    ### Creating a new entry, but we don't know what RGB LED to use yet, so NO_IDX
+                    last_ad_by_key[addr_text] = (ad, now_ns, ad.rssi, NO_IDX)
+        delete_very_old(rows, last_ad_by_key)
+        hide_old(last_ad_by_key, time.monotonic_ns() - hide_time_ns)
+        remove_old(last_ad_by_key, time.monotonic_ns() - stale_time_ns)
+        update_screen(rows, last_ad_by_key, now_ns)
