496471e06e60989d077a36d04edcb4b0507fe3c4
David Glaude <dglaude@users.noreply.github.com>
2020-10-18 23:28:45 +0200

    Update not_as_scanner_clue_wip.py
    Further removal of unecessary code. Still not a Scanner, nor a library, just a demo.

--- not_as_scanner_clue_wip.py
+++ not_as_scanner_clue_wip.py
@@ -16,10 +16,8 @@ import random
 import board
 from adafruit_clue import clue
 import displayio
-from adafruit_ble import BLERadio
-from adafruit_ble.advertising.adafruit import AdafruitColor
 
-MAX_COLOR = 223
+MAX_COLOR = 225
 
 COLOR_TRANSPARENT_INDEX = 0
 COLOR_OFFWHITE_INDEX = 1
@@ -44,77 +42,41 @@ max_color = [1, 5, 10, 17, 26, 37, 65, 101, 145, 226]
 side_color = [1, 2, 3, 4, 5, 6 , 8, 10, 12, 15]
 scale_color = [240, 120, 80, 60, 48, 40, 30, 24, 20, 16]
 
-# Create a bitmap with two colors + 36 colors for the map
-bitmap = displayio.Bitmap(6, 6, 36 + 2)
 
-##### Create a bitmap with two colors + 64 colors for the map
-####bitmap = displayio.Bitmap(8, 8, 64 + 2)
-
-
-def old_prefil_bitmap():
+"""
+Filling using a growing pattern.
+Only works on square.
+Each position get a unique palette colour.
+"""
+def prefil_bitmap(my_bitmap):
+#    print(bitmap.width, bitmap.height)
     val = 2
-    for i in range(0, bitmap.height):
-        for j in range(0, bitmap.width):
-            bitmap[i, j] = val
-            val = val + 1
-
-
-def working_prefil_bitmap():
-    val = 2
-    for limit in range(0, bitmap.width):
-        for i in range(0, limit):
-            bitmap[i, limit] = val
-            val = val + 1
-        bitmap[limit, limit] = val
-        val = val + 1
-        for j in range(0, limit):
-            bitmap[limit, j] = val
-            val = val + 1
-
-
-def ok_progressive_prefil_bitmap():
-    val = 2
-    for limit in range(0, bitmap.width):
-        for i in range(0, limit):
-            bitmap[i, limit] = val
-            val = val + 1
-        for j in range(limit, -1, -1):
-            bitmap[limit, j] = val
-            val = val + 1
-
-
-def prefil_bitmap():
-    print(bitmap.width, bitmap.height)
-    val = 2
-    for limit in range(0, bitmap.width):
+    for limit in range(0, my_bitmap.width):
         if (limit % 2) == 0:
             for i in range(0, limit):
-                bitmap[i, limit] = val
+                my_bitmap[i, limit] = val
                 val = val + 1
             for j in range(limit, -1, -1):
-                bitmap[limit, j] = val
+                my_bitmap[limit, j] = val
                 val = val + 1
         else:
             for i in range(0, limit):
-                bitmap[limit, i] = val
+                my_bitmap[limit, i] = val
                 val = val + 1
             for j in range(limit, -1, -1):
-                bitmap[j, limit] = val
+                my_bitmap[j, limit] = val
                 val = val + 1
 
 
-# Create a 8*8 bitmap pre-filled with 64 colors (color 0 and 1 are reserved)
-prefil_bitmap()
+
+# Create a bitmap with two colors + 36 colors for the palette_mapping
+bitmap = displayio.Bitmap(6, 6, 36 + 2)
+
+# Pre-fill the bitmap with 38 colors (color 0 and 1 are reserved)
+prefil_bitmap(bitmap)
 
 # Create an empty palette that will be used in one to one mapping
-palette_mapping = displayio.Palette(MAX_COLOR + 2)
-
-palette_mapping[0] = 0x000000
-palette_mapping[1] = 0xFFFFFF
-
-color_select_palette = displayio.Palette(len(color_options))
-for i, color in enumerate(color_options):
-    color_select_palette[i] = color
+palette_mapping = displayio.Palette(MAX_COLOR + 3)
 
 
 def make_transparent():
@@ -135,52 +97,45 @@ def make_black():
 
 
 def make_palette():
+    palette_mapping[0] = 0x000000
+    palette_mapping[1] = 0xFFFFFF
     for i, color in enumerate(nearby_colors): ### This might crash if above 100 ###
         palette_mapping[i+2] = color
 
 
 # Create a TileGrid using the Bitmap and Palette
 tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette_mapping)
-
 patchwork_group = displayio.Group(scale=30)
-
 patchwork_group.append(tile_grid)
-
 # Create a Group
 group = displayio.Group()
-
 # Add the TileGrid to the Group
 group.append(patchwork_group)
-
 # Add the Group to the Display
 display.show(group)
 
-cur_color = 0
 
-nearby_addresses = ["myself"]
-
-nearby_colors = [color_options[cur_color]]
+nearby_colors = [0x888888]
 
 max_color = [1, 5, 10, 17, 26, 37, 65, 101, 145, 226]
 side_color = [1, 2, 3, 4, 5, 6 , 8, 10, 12, 15]
 scale_color = [240, 120, 80, 60, 48, 40, 30, 24, 20, 16]
 
-best_resolution = -1
 
+best_resolution = -1
 def adapt_resolution(to_fit):
     global best_resolution, patchwork_group, tile_grid, bitmap
-
+    # Check the best resolution we should use for 'to_fit' element.
     best = 0
     for i, max_c in enumerate(max_color):
         best = i
         if max_c > to_fit:
             break
-
-    # Rebuild a new bitmap scale
+    # Rebuild a new bitmap scale if needed
     if best_resolution!=best:
         group.remove(patchwork_group)
         bitmap = displayio.Bitmap(side_color[best], side_color[best], max_color[best] + 1)
-        prefil_bitmap()
+        prefil_bitmap(bitmap)
         tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette_mapping)
         patchwork_group = displayio.Group(scale=scale_color[best])
         patchwork_group.append(tile_grid)
@@ -200,18 +155,9 @@ def draw_grid():
 
 
 def add_fake():
-    fake_mac = ''.join([random.choice("0123456789abcdef") for n in range(10)])
-    while fake_mac in nearby_addresses:
-        fake_mac = ''.join([random.choice("0123456789abcdef") for n in range(10)])
     fake_color = random.choice(color_options)
-    nearby_addresses.append(fake_mac)
     nearby_colors.append(fake_color)
 
-
-make_white()
-draw_grid()
-
-time.sleep(1)
 
 make_black()
 draw_grid()
